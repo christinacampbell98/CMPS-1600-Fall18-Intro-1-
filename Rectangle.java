c1f5a9db7c2e0a1df526cc03721e6b1cfa193e7a
christinacampbell98 <43076993+christinacampbell98@users.noreply.github.com>
2018-09-24 17:29:08 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -24,39 +24,42 @@ public class Rectangle
         //this becomes the ID for each rectangle//
         this.rectangleID=count;
     }
-
+// create a method that sets the height variable
     public void setHeight ( double h){
 
         this.height = h;
     }
+    //create a get method for height that returns the assigned height 
     public double getHeight () {
         return this.height;
     }
+    //create a set method for length
     public void setLength ( double l){
 
         this.length = l;
     }
+    //create a get method for length that returns the assigned length
     public double getLength () {
 
         return this.length;
     }
-
+//calculate the area and return it as a double
     public double area() {
         area= length*height;
         return area;
     }
-
+//calculate the perimeter and return it as a double 
     public double perimeter() {
         perimeter= (2*height) + (2*length);
         return perimeter;
     }
 
 
-
+//create a get method for the RectangleID that returns the certain ID
     public int getRectangleID(){
         return rectangleID;
     }
-
+// this method displays information about the rectangle to the user as a string 
     public String toString() {
 
         String rectInfo=("This is a rectangle of dimension " + getHeight() + " by "+ getLength() + ".");
